1c5b2d16238609ae44f893e2281512eb5deef3ea
Clement Helsens <clementhelsens>
2025-11-08 09:42:56 +0100

    add image name

--- somiteCounting/evaluate.py
+++ somiteCounting/evaluate.py
@@ -20,11 +20,11 @@ def load_and_prepare_image(img_path, resize=(224,224)):
     img_tensor = torch.from_numpy(np.array(img_pil).astype(np.float32)/65535.0).unsqueeze(0).unsqueeze(0)
     return img_raw, img_tensor
 
-def show_image_prediction(img_raw, gt_total, gt_def, pred_total, pred_def):
+def show_image_prediction(img_raw, gt_total, gt_def, pred_total, pred_def, img_name=""):
     plt.figure(figsize=(6,6))
     plt.imshow(img_raw, cmap="gray", vmin=0, vmax=1)
     plt.axis("off")
-    plt.title(f"GT: total={gt_total}, defective={gt_def}\nPred: total={pred_total:.1f}, defective={pred_def:.1f}")
+    plt.title(f"image name: {img_name}\nGT: total={gt_total}, defective={gt_def}\nPred: total={pred_total:.1f}, defective={pred_def:.1f}")
     plt.show()
 
 # -----------------------------
@@ -68,7 +68,7 @@ def evaluate_folder(img_dir, label_dir, checkpoint_path, save_csv=None, device=N
         pred_total, pred_def = pred
 
         # Display
-        show_image_prediction(img_raw, gt_total, gt_def, pred_total, pred_def)
+        show_image_prediction(img_raw, gt_total, gt_def, pred_total, pred_def, img_name=img_name)
 
         # Store results
         results.append({
