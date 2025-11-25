c4705be980d13d53e696209526e04cf014b3a0b4
Clement Helsens <clementhelsens>
2025-11-25 13:53:03 +0100

    add template drugs

--- well_explorer/urls.py
+++ well_explorer/urls.py
@@ -6,6 +6,6 @@ from django.views.generic.base import RedirectView
 
 urlpatterns = [
     path(r"", views.index, name="index"),
-    path(r"", views.sortable_table, name="drugs_listing"),
+    path(r"drugs_listing", views.sortable_table, name="drugs_listing"),
     path(r"bokeh_dashboard", views.bokeh_dashboard, name="bokeh_dashboard"),
 ]
