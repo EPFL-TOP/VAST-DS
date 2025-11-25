71158f32af7af39d4fffe5cfa34b1447e501289b
Clement Helsens <clementhelsens>
2025-11-25 13:55:08 +0100

    add template drugs

--- well_explorer/views.py
+++ well_explorer/views.py
@@ -1157,4 +1157,4 @@ from django.shortcuts import render
 
 def sortable_table(request):
     #records = Record.objects.all()
-    return render(request, "drugs_listing.html", {"records": []})
+    return render(request, "well_explorer/drugs_listing.html", {"records": []})
