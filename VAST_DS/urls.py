0b2dbd81ba6e35db9083a6ff320b8019efc7ecd4
Clement Helsens <clementhelsens>
2026-01-14 12:04:43 +0100

    add images

--- VAST_DS/urls.py
+++ VAST_DS/urls.py
@@ -48,6 +48,11 @@ urlpatterns = [
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
 bokeh_apps = [
     autoload("well_mapping/bokeh_dashboard", views_wm.vast_handler),
     autoload("well_explorer/bokeh_dashboard", views_we.vast_handler),
