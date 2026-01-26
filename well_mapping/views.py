effb10f5e53a2df7be288201a8ed6deff8e9a179
Clement Helsens <clementhelsens>
2026-01-26 15:58:01 +0100

    try hoover tool

--- well_mapping/views.py
+++ well_mapping/views.py
@@ -229,7 +229,7 @@ def vast_handler(doc: bokeh.document.Document) -> None:
 
     plot_wellplate_source.add_layout(labels)
 
-    hover_grid = bokeh.models.HoverTool(tooltips=[("ID", "@id"), ("(Row,Col)", "(@x{}, @y{})")])
+    hover_grid = bokeh.models.HoverTool(tooltips=[("Drugs", "@drug"), ("(Col,Row)", "(@x, @y)")])
     hover_grid.mode = 'mouse' 
     hover_grid.point_policy = 'snap_to_data'
     plot_wellplate_source.add_tools(hover_grid)
