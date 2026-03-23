0e4d930a35a75231ae8f5dfe642b63143a1344ea
Clement Helsens <clementhelsens>
2026-03-23 20:46:53 +0100

    add plot

--- makeplotdrug.py
+++ makeplotdrug.py
@@ -1,5 +1,6 @@
 import os
 import django
+import matplotlib.pyplot as plt
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "VAST_DS.settings")  # <-- change this
 django.setup()
@@ -15,10 +16,29 @@ dest=DestWellPropertiesPredicted.objects.filter(
         dest_well__source_well__drugs__derivation_name=derivation_name,
         dest_well__well_plate__experiment__name__in=experiment_names,
     ).distinct()
-
+n_total = []
+n_bad = []
 for d in dest:
     #print('-------------- ',len(d))
     print('-------------- ',d)
+    n_total.append(d.n_total_somites)
+    n_bad.append(d.n_bad_somites)
+
+
+
+plt.figure(figsize=(10, 5))
+
+# Histogram for total somites
+plt.hist(n_total, bins=20, alpha=0.6, label="Total somites")
+
+# Histogram for bad somites
+plt.hist(n_bad, bins=20, alpha=0.6, label="Bad somites")
+
+plt.xlabel("Number of somites")
+plt.ylabel("Frequency")
+plt.title(f"Distribution of somites for {derivation_name}")
+plt.legend()
+plt.show()
 
 #qs = (
 #    DestWellPropertiesPredicted.objects
