0815e199dcfb74c25da9ea24380f77eb6e95248a
Clement Helsens <clementhelsens>
2026-03-23 17:14:26 +0100

    add plot

---
+++ makeplotdrug.py
@@ -0,0 +1,24 @@
+from well_mapping.models import DestWellPropertiesPredicted
+
+derivation_name = "LY411575 Stock1"
+experiment_names = ["VAST_2026-01-26", "VAST_2026-02-02"]
+
+
+dest=DestWellPropertiesPredicted.objects.filter(
+        valid=True,
+        dest_well__source_well__drugs__derivation_name=derivation_name,
+        dest_well__well_plate__experiment__name__in=experiment_names,
+    )
+
+for d in dest:
+    print(dest)
+
+qs = (
+    DestWellPropertiesPredicted.objects
+    .filter(
+        valid=True,
+        dest_well__source_well__drugs__derivation_name=derivation_name,
+        dest_well__well_plate__experiment__name__in=experiment_names,
+    )
+    .values("n_total_somites", "n_bad_somites")
+)
